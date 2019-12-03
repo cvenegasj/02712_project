27ea3bde632fb0df1cd0ab916124e23e8817d6ee
Carlos Venegas <cvj_2311@hotmail.com>
2019-12-02 19:19:35 -0500

    Add files via upload

---
+++ js/cell.js
@@ -0,0 +1,9 @@
+class Cell {
+
+    constructor(id, area, perimeter, sites) {
+        this.id = id;
+        this.area = area;
+        this.perimeter = perimeter;
+        this.sites = sites;
+    }
+}
---
+++ js/site.js
@@ -0,0 +1,10 @@
+class Site {
+
+    constructor(i, j, cellIdentity, cellType, activityValue) {
+        this.i = i;
+        this.j = j;
+        this.cellIdentity = cellIdentity;
+        this.cellType = cellType; // integer
+        this.activityValue = activityValue;
+    }
+}
